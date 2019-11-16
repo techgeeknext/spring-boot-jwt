d6a14bd431b65e74cca7a3d5c4dbe3456bc99e22
TechGeekNeXt <techgeeknext@gmail.com>
2019-11-16 23:54:35 +0530

    https://www.techgeeknext.com/spring/spring-boot-security-token-authentication-jwt

---
+++ src/main/java/com/techgeeknext/service/JwtUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.techgeeknext.service;
+
+import java.util.ArrayList;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		if ("techgeeknext".equals(username)) {
+			return new User("techgeeknext", "$2a$10$ixlPY3AAd4ty1l6E2IsQ9OFZi2ba9ZQE0bP7RFcGIWNhyFrrT3YUi",
+					new ArrayList<>());
+		} else {
+			throw new UsernameNotFoundException("User not found with username: " + username);
+		}
+	}
+
+}
